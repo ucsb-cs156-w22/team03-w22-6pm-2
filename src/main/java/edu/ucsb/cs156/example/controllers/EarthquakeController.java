0a9f2a0d743804f6fa6186f66732b001211b1229
ericwu323 <eric323@umail.ucsb.edu>
2022-02-27 15:11:45 -0800

    trying to implement the retrieve endpoint for earthquakes,but there is an error when i import earthquakequeryservice

--- src/main/java/edu/ucsb/cs156/example/controllers/EarthquakeController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/EarthquakeController.java
@@ -2,7 +2,7 @@ package edu.ucsb.cs156.example.controllers;
 
 import edu.ucsb.cs156.example.collections.EarthquakesCollection;
 import edu.ucsb.cs156.example.documents.EarthquakeFeature;
-//import edu.ucsb.cs156.example.services.EarthquakeQueryService;
+import edu.ucsb.cs156.example.services.EarthquakeQueryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -32,6 +32,24 @@ public class EarthquakeController extends ApiController {
     @Autowired
     ObjectMapper mapper;
 
+    @Autowired
+    EarthquakeQueryService earthquakeQueryService;
+    
+    @ApiOperation(value = "Get earthquakes within a certain distance of UCSB's Storke Tower and above a certain magnitude, and add them to the MongoDB database")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/retrieve")
+    public ResponseEntity<String> retrieveEarthquakes(
+            @ApiParam("distance in km, e.g. 100") @RequestParam String dist,
+            @ApiParam("minimum magnitude, e.g. 2.5") @RequestParam String minMag
+        ) throws JsonProcessingException {
+            log.info("retrieveEarthquakes: dist={} minMag={}", dist, minMag);
+            String results = earthquakeQueryService.getJSON(dist, minMag);
+            EarthquakesCollection collection = mapper.readValue(results, EarthquakesCollection.class);
+            List<EarthquakeFeature> features = collection.getFeatures();
+            earthquakeCollection.saveAll(features);
+
+            return ResponseEntity.ok().body(results);
+    }
     @ApiOperation(value = "List all earthquakes")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
@@ -41,7 +59,7 @@ public class EarthquakeController extends ApiController {
     }
     @ApiOperation(value = "Purge all earthquakes")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping("/purge")
+    @PostMapping("/purge")
     public void purgeAll() {
         earthquakeCollection.deleteAll();
     }
