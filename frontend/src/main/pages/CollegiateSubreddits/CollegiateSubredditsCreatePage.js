e6a00180b0671b90001dcfb36f7a89c58d957d07
NingXia <ningxia@ucsb.edu>
2022-02-27 23:19:52 -0800

    nx - copied team02 CollegiateSubreddit files, implemented List page and Create page, added delete functionality

--- frontend/src/main/pages/CollegiateSubreddits/CollegiateSubredditsCreatePage.js
+++ frontend/src/main/pages/CollegiateSubreddits/CollegiateSubredditsCreatePage.js
@@ -1,13 +1,50 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import CollegiateSubredditsForm from "main/components/CollegiateSubreddits/CollegiateSubredditsForm";
+import { Navigate } from 'react-router-dom';
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from 'react-toastify';
+import { Col } from "react-bootstrap";
+
 
 export default function CollegiateSubredditsCreatePage() {
+
+  const objectToAxiosParams = (collegiateSubreddit) => ({
+    url: "/api/CollegiateSubreddits/post",
+    method: "POST",
+    params: {
+      name: collegiateSubreddit.name,
+      location: collegiateSubreddit.location,
+      subreddit: collegiateSubreddit.subreddit
+    }
+  });
+
+  const onSuccess = (collegiateSubreddit) => {
+    toast(`New collegiateSubreddit Created - id: ${collegiateSubreddit.id} name: ${collegiateSubreddit.name}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/CollegiateSubreddits/all"]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/CollegiateSubreddits/list" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Creat new CollegiateSubreddit</h1>
-        <p>
-          This is where the create page will go
-        </p>
+        <h1>Create New CollegiateSubreddit</h1>
+
+        <CollegiateSubredditsForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   )
