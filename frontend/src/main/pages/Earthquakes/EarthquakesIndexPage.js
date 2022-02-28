4c0f852f40848cce5945ac938bb05d29664ee1d9
ericwu323 <eric323@umail.ucsb.edu>
2022-02-28 03:18:04 -0800

    purge button earthquakeindexpage

--- frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
+++ frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
@@ -1,10 +1,13 @@
 
 import React from 'react'
-import { useBackend } from 'main/utils/useBackend';
+import { useBackend, useBackendMutation } from 'main/utils/useBackend';
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import EarthquakesTable from 'main/components/Earthquakes/EarthquakesTable';
 import { useCurrentUser } from 'main/utils/currentUser'
+import { Button } from 'react-bootstrap';
+import { toast } from "react-toastify";
+
 
 export default function EarthquakesIndexPage() {
 
@@ -18,11 +21,26 @@ export default function EarthquakesIndexPage() {
       []
     );
 
+    function Purge(){
+      let purge = useBackendMutation(
+          () => ({ url: "/api/earthquakes/purge", method: "purge" }),
+          { onSuccess: () => { toast("Successfully purged earthquakes!"); } },
+          // Stryker disable next-line all : don't test internal caching of React Query
+          ["/api/earthquakes/all"],
+        );
+        return (
+          <Button onClick={ () => { purge.mutate(); } } data-testid="purge">
+            Purge
+          </Button>
+        );
+      }
+    }
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Students</h1>
         <EarthquakesTable earthquakes={earthquakes} currentUser={currentUser} />
+        <Purge/>
       </div>
     </BasicLayout>
   )
